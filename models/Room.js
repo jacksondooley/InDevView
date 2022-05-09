01d46afe689378f1bdd27412bddd3783252b61b2
wcorona269 <wcorona269@gmail.com>
2022-05-09 11:05:05 -0400

    create Room model

---
+++ models/Room.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const RoomSchema = new Schema({
+    host_id: {
+			type: Number,
+			require: true
+		},
+		room_key: {
+			type: Number,
+			require: true
+		},
+		participants: [{
+			type: Schema.Types.ObjectId,
+			ref: "User"
+		}],
+		questions: {
+        type: Array,
+        required: true
+    },
+    date: {
+			type: Date,
+			default: Date.now
+	}
+});
+
+module.exports = Room = mongoose.model('Room', RoomSchema)
