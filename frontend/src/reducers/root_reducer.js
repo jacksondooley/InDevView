5100c57ede0fc342e9df250169bd3fdb76538866
Jackson <jacksondooley24@gmail.com>
2022-05-09 16:35:43 -0400

    merged rooms-redux

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,20 +1,14 @@
 import { combineReducers } from 'redux';
 import session from './session_api_reducer';
 import errors from './errors_reducer'
-<<<<<<< HEAD
 import questions from './questions_reducer'
-=======
 import rooms from './rooms_reducer'
->>>>>>> origin/rooms-redux
 
 const RootReducer = combineReducers({
   session,
   errors,
-<<<<<<< HEAD
-  questions
-=======
-  rooms
->>>>>>> origin/rooms-redux
+  questions,
+//   rooms
 });
 
 export default RootReducer;
