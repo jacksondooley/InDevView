38df630adcde23c35f31ce0756c43c89c36864f3
Jackson <jacksondooley24@gmail.com>
2022-05-08 21:24:16 -0400

    working frontend routes

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>InDevView</h1>
+        <footer>
+          Copyright &copy; 2021 InDevView
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
