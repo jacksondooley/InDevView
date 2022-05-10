dbb0be00f8bebd36ee0adab17d3af3eae0dac7b3
wcorona269 <wcorona269@gmail.com>
2022-05-10 10:20:11 -0400

    add participant function almost done

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app = express()
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io")
-const io = new Server(server)
-
+const io = new Server(server);
 const db = require('./config/keys').mongoURI
 const mongoose = require('mongoose')
 const users = require("./routes/api/users")
--- controllers/rooms_controller.js
+++ controllers/rooms_controller.js
@@ -1,3 +1,4 @@
+const { render } = require("express/lib/response");
 const Room = require("../models/Room");
 const User = require("../models/User");
 
@@ -22,7 +23,38 @@ const fetchRoom = (req, res) => {
 	})
 }
 
+const addParticipant = (req, res) => {
+	Room.find({ room_key: req.params.room_key }, (err, targetRoom) => {
+		if (err) {console.log(`this room does not exist`)}
+		else {
+			// const index = targetRoom.participants.indexOf(req.user.id);
+			// if (index > -1) {
+				targetRoom[0].participants.push(req.body.user)
+				// targetRoom.save();
+				
+				res.json(targetRoom)
+				// console.log(targetRoom[0].participants)
+					// targetRoom.participants.push(req.body.user);
+			// } else {
+			// 	console.log(`You can't join a room twice!`)
+			// }
+			// targetRoom.save()
+		}
+	})
+}
+
+// const removeUserFromGroup = (req, res) => {
+// 	Group.findById(req.params.groupId, (err, groupResult) => {
+// 			const index = groupResult.users.indexOf(req.user.id);
+// 			if (index > -1) {
+// 					groupResult.users.splice(index, 1);
+// 			}
+// 		}
+// 	)
+// }
+
 module.exports = {
 		createRoom,
-		fetchRoom
+		fetchRoom,
+		addParticipant
 };
