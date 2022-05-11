2dc486563acaadcb10a50c24a817b22b863acb31
Spencer-JLee <spencer.lee.careers@gmail.com>
2022-05-11 15:37:37 -0400

    work on interview room

--- frontend/src/components/editor/editor.js
+++ frontend/src/components/editor/editor.js
@@ -5,13 +5,64 @@ import '../../stylesheets/editor.css'
 class CodeEditor extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            answer: '',
+            parameters: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.extractParameters = this.extractParameters.bind(this);
+        this.extractCode = this.extractCode.bind(this);
     }
 
     handleChange(value, event){
-        //value is a string
+        const parameters = this.extractParameters(value);
+        const answer = this.extractCode(value)
+        this.setState({ answer: answer, parameters: parameters})
     }
 
+    extractParameters(str){
+        let newStr = '';
+        let parameters = false;
+        for(let i = 0; i < str.length; i++){
+            if(str[i] === "("){
+                parameters = true
+            }
+            else if(str[i] === ")"){
+                break
+            }
+            else if(parameters){
+                newStr += str[i]
+            }
+            
+        }
+        return newStr.replace(',', '').split(' ');
+    }
 
+    extractCode(str){
+        let newStr = '';
+        let code = false;
+        let lastRightBracket = str.length;
+
+        for(let i = str.length-1; i > -1; i--){
+            if(str[i] === "}"){
+                lastRightBracket = i;
+                break;
+            }
+        }
+
+        for(let i = 0; i < str.length; i++){
+            if(str[i] === "{"){
+                code = true;
+            }
+            else if(i === lastRightBracket){
+                break
+            }
+            else if(code){
+                newStr += str[i];
+            }
+        }
+        return newStr
+    }
 
     render(){
         return(
@@ -23,6 +74,7 @@ class CodeEditor extends React.Component{
                     theme='vs-dark'
                     onChange={this.handleChange}
                 />
+                
             </div>
         )
     }
