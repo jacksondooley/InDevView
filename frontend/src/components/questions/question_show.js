b560b84f381233daa96e8d6a25966dfbbe66d91a
wcorona269 <wcorona269@gmail.com>
2022-05-11 18:02:42 -0400

    USE THIS COMMIT

--- frontend/src/components/questions/question_show.js
+++ frontend/src/components/questions/question_show.js
@@ -6,13 +6,12 @@ import { fetchQuestion } from "../../actions/question_actions";
 const QuestionShowContainer = (props) => {
 	const dispatch = useDispatch()
 	const questions = useSelector(state => state.questions)
-	const [question, setQuestion] = useState()
-	useEffect(() => {
-		dispatch(fetchQuestion(props.match.params.id))
-	}, [])
+	// const [question, setQuestion] = useState()
+	// useEffect(() => {
+	// 	dispatch(fetchQuestion(props.match.params.id))
+	// }, [])
 
 	console.log(props.match.params.id) // => "627ac4c623ab85a880964d47"
-	console.log(question)
 	// if (questions)
 	return (
 		<>
