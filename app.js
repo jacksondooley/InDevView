75b501d85d8a1f5a0d421835ce7dea95d5e64095
Jackson <jacksondooley24@gmail.com>
2022-05-13 02:07:44 -0400

    added heroku changes to backend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const path = require('path');
 
 
 
@@ -16,12 +17,12 @@ const { join } = require("path");
 const cors = require('cors')
 
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('frontend/build'));
-//     app.get('/', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//     })
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 mongoose
     .connect(db, { useNewUrlParser: true})
